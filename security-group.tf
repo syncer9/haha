eb62c4807a15aefaed9de92773a2f5a5af1adb2d
jkkang <syncer9@gmail.com>
2021-12-01 21:18:37 +0900

    th

--- security-group.tf
+++ security-group.tf
@@ -31,6 +31,13 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port = 1433
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
